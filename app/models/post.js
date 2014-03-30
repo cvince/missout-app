75e741ed9f2951f6354282e42829efc6a5fbd772
Dan Heidel <dan.heidel@gmail.com>
2014-03-29 21:20:06 -0700

    added routeFactory and mangled db models

--- app/models/post.js
+++ app/models/post.js
@@ -1,24 +1,27 @@
 'use strict';
 
-module.exports = function (Schema, mongoose) {
-  var CommentSchema = new Schema({
-    author    : { type: Schema.ObjectID },
-    body      : { type: String },
-    timestamp : { type: Date },
-    tempname  : { type: String }
-  });
+var mongoose    = require('mongoose');
+var Schema      = mongoose.Schema;
 
-  var Comment = mongoose.model ('Comment', CommentSchema);
+var CommentSchema = mongoose.Schema({
+  //author    : { type: Schema.ObjectID },
+  author    : { type: String },
+  body      : { type: String },
+  timestamp : { type: Date },
+  tempname  : { type: String }
+});
 
-  var PostSchema = new Schema({
-    timestamp : { type: Date },
-    loc       : { type: Point, coordinates: [] },
-    author    : { type: Schema.ObjectID },
-    body      : { type: String },
-    comments  : [ Comment ],
-    tempname  : { type: String },
-    tempnames : [{ type: String }]
-  });
+var Comment = mongoose.model('Comment', CommentSchema);
 
-  return mongoose.model('Post', PostSchema);
-};
+var PostSchema = mongoose.Schema({
+  timestamp : { type: Date },
+  //loc       : { type: Point, coordinates: [] },
+  //author    : { type: Schema.ObjectID },
+  author    : { type: String },
+  body      : { type: String },
+  comments  : [ Comment ],
+  tempname  : { type: String },
+  tempnames : [{ type: String }]
+});
+
+module.exports = mongoose.model('Post', PostSchema);
--- server.js
+++ server.js
@@ -47,6 +47,10 @@ require('./app/passport/passport')(passport);
 /* Get Routes */
 require('./app/routes/authenticate')(app, passport) ;
 
+var routeFactory = require('./app/routes/routeGenerator').routeFactory;
+routeFactory('/api/v1/users', '../models/user', app);
+routeFactory('/api/v1/posts', '../models/post', app);
+
 /* Render the index */
 // app.get('/', function (req, res) {
 //   res.sendfile('app/views/index.html');
