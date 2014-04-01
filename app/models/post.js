9c801ae8c3bc2bc1dd73f5dc1205c1a3e6574a85
Vincent Chan <vincent@vincentcchan.com>
2014-03-31 20:41:03 -0700

    updated post schema to have author type: Object

--- app/models/post.js
+++ app/models/post.js
@@ -4,7 +4,7 @@ var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
 var CommentSchema = mongoose.Schema({
-  author    : { type: Schema.ObjectId },
+  author    : { type: Object },
   body      : { type: String },
   timestamp : { type: Date },
   tempname  : { type: String }
