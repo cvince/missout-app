8e6f916a70288252d9e0436685b6e7a6b77602b5
Dan Heidel <dan.heidel@gmail.com>
2014-04-01 16:50:20 -0700

    remade public/app.js

--- public/app.js
+++ public/app.js
@@ -3,6 +3,238 @@
 var App = {};
 
 'use strict';
+/*global alert*/
+/*global App*/
+
+/*
+-- App.locator --
+Keeps track of the user's last known position.
+Coordinates are stored in an array as follows:
+[ longitude, latitude ]
+A timestamp is also kept denoting the last
+time a geolocation was successfully fetched.
+Events:
+  * dispatches 'new-location' when a new geo
+    object is received
+Functions:
+  * getLoc(cb) Updates the user's geolocation
+    and fires the DOM event 'new-location' with
+    user's location
+  * locAge() Returns time since last lookup in
+    seconds
+  * showLoc() Simply returns the stored
+    coordinates without performing another
+    lookup
+  * locStatus() returns object with bool of
+    location validity and the location accuracy
+ */
+function Locator () {
+  var userLoc = {
+    lat: null,
+    lon: null,
+    accuracy: null,
+    timestamp: null
+  };
+  var lastGoodLoc;
+  var maximumAccuracy = 1000;
+  var positionOptions = {
+    enableHighAccuracy: false,
+    timeout: 10000,
+    maximumAge: 10000
+  };
+
+  function Constructor () { }
+
+  Constructor.prototype.getLoc = function (maxAge, maxAccuracy, cb) {
+    if (typeof arguments[0] === "function") {
+      cb = arguments[0];
+      maxAccuracy = 5000;
+      maxAge = 600000;
+    }
+    if (maxAge) {
+      positionOptions.maximumAge = maxAge;
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getPositionData, getPositionError, positionOptions);
+    } else {
+      alert('Your browser does not support geolocation.');
+    }
+
+    function getPositionData (position) {
+      userLoc = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        accuracy: position.coords.accuracy,
+        timestamp: position.timestamp
+      };
+      var event = new CustomEvent('new-location', { detail: userLoc });
+      document.dispatchEvent(event);
+      console.log(position);
+      if(userLoc.accuracy < maxAccuracy){
+        //cache the last userLoc of sufficient accuracy
+        lastGoodLoc = userLoc;
+      }
+      if (cb) {
+        cb(userLoc);
+      }
+    }
+
+    function getPositionError (error){
+      console.log(error);
+    }
+  };
+
+  Constructor.prototype.locAge = function () {
+    return Date.now() - userLoc.timestamp;
+  };
+
+  Constructor.prototype.showLoc = function () {
+    return {
+      userLoc: userLoc,
+      lastGoodLoc: lastGoodLoc
+    };
+  };
+
+  return new Constructor();
+}
+
+App.locator = new Locator();
+
+'use strict';
+/*global App*/
+
+/*
+-- App.postman --
+Handles the fetching, storing and rendering of
+all posts. The REST endpoint is passed as an
+argument when it is initialized.
+Events:
+  * listens for 'new-location' and fetches a new
+    feed
+  * dispatches 'feedJSON' once the data for the
+    new feed is obtained
+Functions:
+  * newFeed(loc) expects a point in the format
+    { lon: Num, lat: Num } and sends the request
+    to the feed endpoint
+  * post(data, cb) creates a new post with the
+    JSON in data param, and takes a callback
+ */
+function Postman (endpoint) {
+  var url = endpoint,
+      models,
+      feed;
+  function Constructor () { }
+  Constructor.prototype.XHR = function (method, data, url, async, cb) {
+    var req = new XMLHttpRequest();
+    req.open(method, url, async);
+    req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    //req.responseType = '';
+    req.onload = function () {
+      if (req.status >= 200 && req.status < 400) {
+        models = JSON.parse(req.responseText);
+        cb(models);
+      } else {
+        return false;
+      }
+    };
+    req.onerror = function (err) {
+      console.log('XHR Error: ' + JSON.stringify(err));
+    };
+    if (data) {
+      console.log("bad data: " + JSON.stringify(data));
+      req.send(JSON.stringify(data));
+    } else {
+      req.send();
+    }
+  };
+
+  Constructor.prototype.fetch = function (cb) {
+    return this.XHR('GET', null, url, true, cb);
+  };
+
+  Constructor.prototype.show = function () {
+    return models;
+  };
+
+  Constructor.prototype.post = function (data, cb) {
+    /* location functionality */
+    return this.XHR('POST', data, url, true, cb);
+  };
+
+  Constructor.prototype.newFeed = function (loc) {
+    console.log('data to newFeed function: ' + JSON.stringify(loc));
+    var req = new XMLHttpRequest(),
+        url = 'http://localhost:3000/api/v1/feed';
+    req.open('POST', url, true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    req.onload = function (d) {
+      feed = JSON.parse(d.currentTarget.responseText);
+      var event = new CustomEvent('feedJSON', feed);
+      document.dispatchEvent(event);
+      console.log('got a feed, check it:');
+      console.log(App.postman.showFeed());
+    };
+    req.onerror = function (err) {
+      console.log(err)
+    };
+//    loc.lon = -122;
+//    loc.lat = 47;
+    var params = "lon="+loc.lon+"&lat="+loc.lat;
+    console.log(params);
+    req.send(params);
+  };
+
+  Constructor.prototype.showFeed = function () {
+    return feed;
+  };
+
+  return new Constructor();
+}
+App.postman = new Postman('http://localhost:3000/api/v1/posts');
+// Receive the DOM event 'feed-location' and query the
+// feed endpoint
+document.addEventListener('new-location', function (e) {
+  console.log('data to new loc event ' + JSON.stringify(e.detail));
+  App.postman.newFeed(e.detail);
+});
+
+
+
+var messageOut = document.getElementById('message-out');
+var posts = document.getElementById('post-display');
+var submit = document.getElementById('submit-post');
+
+submit.addEventListener(function (e) {
+  console.log(e);
+});
+
+var data =  {
+  timestamp : new Date()
+  // author    : { type: Schema.ObjectId },
+  // body      : {  },
+  // comments  : [ Comment ],
+  // tempname  : { type: String },
+  // tempnames : [{ type: String }]
+};
+
+submit.disabled = true;
+
+App.locator.getLoc(function (loc) {
+  console.log('data to page: ' + JSON.stringify(loc));
+  submit.disabled = false;
+  submit.addEventListener('click', function() {
+    var data = {};
+    data.body = messageOut.value.toString();
+    data.loc = { type: "Point", coordinates: [ loc.lon, loc.lat ] };
+    App.postman.post(data, function (res) {
+      console.log('post ok, contents - ' + JSON.stringify(res));
+    })
+  }, false);
+
+});
+'use strict';
 /*global App*/
 
 /*
@@ -56,277 +288,3 @@ function Heartbeat () {
 
 App.heartbeat = new Heartbeat();
 App.heartbeat.startBeat();
-'use strict';
-/*global alert*/
-/*global App*/
-
-/*
--- App.locator --
-Keeps track of the user's last known position.
-Coordinates are stored in an array as follows:
-[ longitude, latitude ]
-A timestamp is also kept denoting the last
-time a geolocation was successfully fetched.
-Events:
-  * dispatches 'new-location' when a new geo
-    object is received
-Functions:
-  * getLoc(cb) Updates the user's geolocation
-    and fires the DOM event 'new-location' with
-    user's location
-  * locAge() Returns time since last lookup in
-    seconds
-  * showLoc() Simply returns the stored
-    coordinates without performing another
-    lookup
-  * locStatus() returns object with bool of
-    location validity and the location accuracy
- */
-function Locator () {
-  var userLoc = {
-    lat: null,
-    lon: null,
-    accuracy: null,
-    timestamp: null
-  };
-  var lastGoodLoc;
-  var maximumAccuracy = 1000;
-  var positionOptions = {
-    enableHighAccuracy: false,
-    timeout: 3000,
-    maximumAge: 10000
-  };
-
-  function Constructor () { }
-
-  Constructor.prototype.getLoc = function (cb, maxAge, maxAccuracy) {
-    if(maxAge){ positionOptions.maximumAge = maxAge; }
-    if(maxAccuracy){ maximumAccuracy = maxAccuracy; }
-
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getPositionData, getPositionError, positionOptions);
-    } else {
-      alert('Your browser does not support geolocation.');
-    }
-
-    function getPositionData (position){
-      console.log(position);
-      userLoc = {
-        lat: position.coords.latitude,
-        lon: position.coords.longitude,
-        accuracy: position.coords.accuracy,
-        timestamp: position.timestamp
-      };
-      if(userLoc.accuracy < maximumAccuracy){
-        //cache the last userLoc of sufficient accuracy
-        lastGoodLoc = userLoc;
-      }
-      if (cb) {
-        cb(userLoc);
-      }
-    }
-
-    function getPositionError (error){
-      console.log(error);
-    }
-  };
-
-  Constructor.prototype.locAge = function () {
-    return Date.now() - userLoc.timestamp;
-  };
-
-  Constructor.prototype.showLoc = function () {
-    return {
-      userLoc: userLoc,
-      lastGoodLoc: lastGoodLoc
-    };
-  };
-
-  return new Constructor();
-}
-
-App.locator = new Locator();
-App.locator.getLoc();
-'use strict';
-/*global App*/
-
-/*
--- App.postman --
-Handles the fetching, storing and rendering of
-all posts. The REST endpoint is passed as an
-argument when it is initialized.
-Events:
-  * listens for 'new-location' and fetches a new
-    feed
-  * dispatches 'feedJSON' once the data for the
-    new feed is obtained
-Functions:
-  * newFeed(loc) expects a point in the format
-    { lon: Num, lat: Num } and sends the request
-    to the feed endpoint
-  * post(data, cb) creates a new post with the
-    JSON in data param, and takes a callback
- */
-function Postman (endpoint) {
-  var url = document.URL + endpoint,
-    models;
-  function Constructor () { }
-  Constructor.prototype.XHR = function (method, data, url, async, cb) {
-    var req = new XMLHttpRequest();
-    req.open(method, url, async);
-    req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    //req.responseType = '';
-    req.onload = function () {
-      if (req.status >= 200 && req.status < 400) {
-        if (req.responseText) {
-          cb(req.responseText);
-        } else {
-          cb();
-        }
-      } else {
-        return false;
-      }
-    };
-    req.onerror = function (err) {
-      console.log('XHR Error: ' + JSON.stringify(err));
-    };
-    if (data) {
-      console.log("bad data: " + JSON.stringify(data));
-      req.send(JSON.stringify(data));
-    } else {
-      req.send();
-    }
-  };
-
-  Constructor.prototype.fetch = function (cb) {
-    return this.XHR('GET', null, url, true, cb);
-  };
-
-  Constructor.prototype.show = function () {
-    return models;
-  };
-
-  Constructor.prototype.post = function (data, cb) {
-    /* location functionality */
-    return this.XHR('POST', data, url, true, cb);
-  };
-
-  Constructor.prototype.newFeed = function (loc) {
-    console.log('data to newFeed function: ' + console.log(loc));
-    this.XHR('GET', [loc.lon, loc.lat], 'localhost:3000/api/v1/feed', true, function (posts) {
-        console.log('Received feed:' + JSON.stringify(posts));
-    });
-  };
-
-  return new Constructor();
-}
-App.postman = new Postman('http://localhost:3000/api/v1/posts');
-// Receive the DOM event 'feed-location' and query the
-// feed endpoint
-document.addEventListener('new-location', function (e) {
-  console.log('data to new loc event ' + JSON.stringify(e.detail));
-  App.postman.newFeed(e.detail);
-});
-
-
-// function microAjax(url, callbackFunction)
-// {
-//   this.bindFunction = function (caller, object) {
-//     return function() {
-//       return caller.apply(object, [object]);
-//     };
-//   };
-
-//   this.stateChange = function (object) {
-//     if (this.request.readyState==4)
-//       this.callbackFunction(this.request.responseText);
-//   };
-
-//   this.getRequest = function() {
-//     if (window.ActiveXObject)
-//       return new ActiveXObject('Microsoft.XMLHTTP');
-//     else if (window.XMLHttpRequest)
-//       return new XMLHttpRequest();
-//     return false;
-//   };
-
-//   this.postBody = (arguments[2] || "");
-
-//   this.callbackFunction=callbackFunction;
-//   this.url=url;
-//   this.request = this.getRequest();
-
-//   if(this.request) {
-//     var req = this.request;
-//     req.onreadystatechange = this.bindFunction(this.stateChange, this);
-
-//     if (this.postBody!=="") {
-//       req.open("POST", url, true);
-//       req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-//       req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-//       req.setRequestHeader('Connection', 'close');
-//     } else {
-//       req.open("GET", url, true);
-//     }
-
-//     req.send(this.postBody);
-//   }
-
-
-
-
-// request = new XMLHttpRequest();
-// request.open('GET', '/my/url', true);
-
-// request.onload = function() {
-//   if (request.status >= 200 && request.status < 400){
-//     // Success!
-//     data = JSON.parse(request.responseText);
-//   } else {
-//     // We reached our target server, but it returned an error
-
-//   }
-// };
-
-// request.onerror = function() {
-//   // There was a connection error of some sort
-// };
-
-// request.send();
-// }
-
-
-
-
-var messageOut = document.getElementById('message-out');
-var posts = document.getElementById('post-display');
-var submit = document.getElementById('submit-post');
-
-submit.addEventListener(function (e) {
-  console.log(e);
-});
-
-var data =  {
-  timestamp : new Date()
-  // author    : { type: Schema.ObjectId },
-  // body      : {  },
-  // comments  : [ Comment ],
-  // tempname  : { type: String },
-  // tempnames : [{ type: String }]
-};
-
-submit.disabled = true;
-
-App.locator.getLoc(function (loc) {
-  console.log('data to page: ' + JSON.stringify(loc));
-  submit.disabled = false;
-  submit.addEventListener('click', function() {
-    var data = {};
-    data.body = messageOut.value.toString();
-    data.loc = { type: "Point", coordinates: [ loc.lon, loc.lat ] };
-    App.postman.post(data, function (res) {
-      console.log('post ok, contents - ' + JSON.stringify(res));
-    })
-  }, false);
-
-});
