86efd68ce093fe01a1a57f006f721fce62d987bb
Dan Heidel <dan.heidel@gmail.com>
2014-04-02 12:17:24 -0700

    reset UI to david branch

--- public/js-ui/button.js
+++ public/js-ui/button.js
@@ -1,37 +1,29 @@
-'use strict';
-
-// Button.prototype.onClicked;
-// Button.prototype.x;
+Button.prototype.normalStyle = 'button';
+Button.prototype.mouseDownStyle = 'buttonClicked';
+Button.prototype.onClicked;
+Button.prototype.x;
 
 function Button() {
 	this.element = document.createElement('div');
 	this.element.setAttribute('class', 'button');
-
-	var normalStyle = 'button';
-	var mouseDownStyle = 'buttonClicked';
-	var isMouseDown;
-	var listeningForScrolling;
-	var scrolling;
-	var activeButton;
-
-	var onMouseDown = function(element, e) {
-		isMouseDown = true;
-		listeningForScrolling = true;
-		scrolling = false;
-		element.setAttribute('class', mouseDownStyle);
-		activeButton = element;
-	};
 	// addElementToDict(this.element, this);
 	// this.element.onclick = function()
 	// {
 	// 	clicked(this);
 	// }
-
 	this.element.addEventListener( 'touchstart' , function touchStart( e ) { onMouseDown(this, e); } , false );
 	this.element.addEventListener( 'touchmove', function touchMove ( e ) { }, false);
 	this.element.addEventListener( 'touchend' , function touchEnd( e ) { onMouseUp(this, e);} , true );
 	this.element.addEventListener('onmouseout', function mouseOut(e) { onMouseOut(this, e); }, true);
 	this.element.addEventListener('onmouseover', function mouseOver(e) { onMouseOver(this, e); }, true);
+}
+
+Button.prototype.onMouseDown = function(element, e) {
+	this.isMouseDown = true;
+	listeningForScrolling = true;
+	scrolling = false;
+	this.element.setAttribute('class', this.mouseDownStyle);
+	activeButton = this;
 }
 
 Button.prototype.onMouseUp = function(element, e) {
