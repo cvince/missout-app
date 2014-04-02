8f9ff46401b83887fbf53b1815c4a8c42387a8ed
Dan Heidel <dan.heidel@gmail.com>
2014-04-02 13:27:26 -0700

    fixing scoping in button.js

--- public/js-ui/button.js
+++ public/js-ui/button.js
@@ -1,32 +1,40 @@
+'use strict';
+/*jslint unused: false*/
+
 Button.prototype.normalStyle = 'button';
 Button.prototype.mouseDownStyle = 'buttonClicked';
-Button.prototype.onClicked;
-Button.prototype.x;
+// Button.prototype.onClicked;
+// Button.prototype.x;
 
 function Button() {
+	var that = this;
 	this.element = document.createElement('div');
 	this.element.setAttribute('class', 'button');
+	this.listeningForScrolling = false;
+	this.scrolling = false;
+	this.isMouseDown = false;
+	this.activeButton = null;
 	// addElementToDict(this.element, this);
 	// this.element.onclick = function()
 	// {
 	// 	clicked(this);
 	// }
-	this.element.addEventListener( 'touchstart' , function touchStart( e ) { onMouseDown(this, e); } , false );
-	this.element.addEventListener( 'touchmove', function touchMove ( e ) { }, false);
-	this.element.addEventListener( 'touchend' , function touchEnd( e ) { onMouseUp(this, e);} , true );
-	this.element.addEventListener('onmouseout', function mouseOut(e) { onMouseOut(this, e); }, true);
-	this.element.addEventListener('onmouseover', function mouseOver(e) { onMouseOver(this, e); }, true);
+	this.element.addEventListener('touchstart' , function touchStart( e ) { that.onMouseDown(e); } , false );
+	this.element.addEventListener('touchmove', function touchMove ( e ) { }, false);
+	this.element.addEventListener('touchend' , function touchEnd( e ) { that.onMouseUp(e);} , true );
+	this.element.addEventListener('onmouseout', function mouseOut(e) { that.onMouseOut(e); }, true);
+	this.element.addEventListener('onmouseover', function mouseOver(e) { that.onMouseOver(e); }, true);
 }
 
-Button.prototype.onMouseDown = function(element, e) {
+Button.prototype.onMouseDown = function(e) {
 	this.isMouseDown = true;
-	listeningForScrolling = true;
-	scrolling = false;
+	this.listeningForScrolling = true;
+	this.scrolling = false;
 	this.element.setAttribute('class', this.mouseDownStyle);
-	activeButton = this;
-}
+	this.activeButton = this;
+};
 
-Button.prototype.onMouseUp = function(element, e) {
+Button.prototype.onMouseUp = function(e) {
 	this.element.setAttribute('class', this.normalStyle);
 	if (this.isMouseDown) {
 		if (this.onClicked) {
@@ -34,19 +42,19 @@ Button.prototype.onMouseUp = function(element, e) {
 		}
 	}
 	this.isMouseDown = false;
-}
+};
 
-Button.prototype.onMouseOut = function(element, e) {
+Button.prototype.onMouseOut = function(e) {
 	this.element.setAttribute('class', this.normalStyle);
-}
+};
 
-Button.prototype.onMouseOver = function(element, e) {
+Button.prototype.onMouseOver = function(e) {
 	if (this.isMouseDown) {
 		this.element.setAttribute('class', this.mouseDownStyle);
 	}
-}
+};
 
 Button.prototype.setLeft = function(left) {
 	this.x = left;
 	this.element.style.left = left + 'rem';
-}
+};
